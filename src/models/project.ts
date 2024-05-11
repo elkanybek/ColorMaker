794e12e07294f8c6c35fbda9947cd20d53ea3699
joeyl83 <2268268@johnabbottcollege.net>
2024-05-11 18:16:22 -0400

    implement feature to get colors from an image link in the model.

--- src/models/project.ts
+++ src/models/project.ts
@@ -6,6 +6,14 @@ import {
 	snakeToCamel,
 } from "../utils";
 
+const PAT = '5134e3389d1e421e8a5c999ae1d6b1f5';
+
+const USER_ID = 'skinnypig';       
+const APP_ID = 'color-maker';
+
+const MODEL_ID = 'color-recognition';
+const MODEL_VERSION_ID = 'dd9458324b4b45c2be1a7ba84d27cd04'; 
+
 export interface ProjectProps {
 	id?: number;
     name: string;
@@ -120,4 +128,35 @@ export default class Project {
 			status: "complete",
 		});
 	}
+
+	async  getImageColors(imageUrl: StaticRangeInit){
+        const raw = JSON.stringify({
+            "user_app_id": {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            "inputs": [
+                {
+                    "data": {
+                        "image": {
+                            "url": imageUrl
+                        }
+                    }
+                }
+            ]
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Key ' + PAT
+            },
+            body: raw
+        };
+
+        const response = await fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions);
+        const jsonObject = await response.json();
+        return jsonObject.outputs[0].data.colors;
+    }
 }
