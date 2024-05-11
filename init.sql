c9b9b8a491e7b8b740eb3ad5d6253889ce9c328e
elkanybek <elkanybek@gmail.com>
2024-05-11 20:19:05 +0000

    push new updates in sql database

--- init.sql
+++ init.sql
@@ -3,16 +3,21 @@ CREATE DATABASE "ColorDB";
 
 \c ColorDB;
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL
-);
+DROP TYPE IF EXISTS project_status;
+CREATE TYPE project_status AS ENUM ('incomplete', 'complete');
+
 
 DROP TABLE IF EXISTS projects; 
 CREATE TABLE projects (
     id SERIAL PRIMARY KEY, 
     name VARCHAR(100), 
-    completed bool 
+    status project_status NOT NULL DEFAULT 'incomplete'
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    project_id INTEGER REFERENCES projects(id) ON DELETE CASCADE
 );
